95e4c97da9a7ab15a9fdc5e4d2c1bd612da70e56
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2023-05-31 16:48:58 -0400

    added Project files for ICE4

---
+++ MDEV1001-M2023-ICE4/MDEV1001-M2023-ICE4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MDEV1001-M2023-ICE4
+//
+//  Created by Tom Tsiliopoulos on 2023-05-31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
